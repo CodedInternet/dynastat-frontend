74ce8febd048b1f6133f2b48c973af85565d4131
Tom Price <tom@codedinternet.com>
2017-08-02 19:27:42 +0100

    Fix opening connections to device using simulator

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,12 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Route.extend({
+  connection: Ember.inject.service('webrtc-controls'),
+
+  setupController(controller, model) {
+    const host = window.location.host;
+    const conn = this.get('connection');
+
+    conn.openConnection(`ws://${host}/ws/signal`);
+  },
 });
